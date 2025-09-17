fa9c82e8cba8bc39411a67e8838c9aa4ebd5cd26
Ben Brougher <Ben Brougher>
2025-09-16 22:49:17 -0700

    wip: actually got a call working into opentui when hitting the lib directly

--- packages/dotnet/Sst.Opentui.Core/Zig.cs
+++ packages/dotnet/Sst.Opentui.Core/Zig.cs
@@ -1,3 +1,10 @@
+using System.Runtime.InteropServices;
+
 namespace Sst.Opentui.Core;
 
-
+//TODO: Make this insternal after testing
+public unsafe static partial class Zig
+{
+    [LibraryImport("opentui", EntryPoint = "createRenderer")]
+    public static partial IntPtr CreateRenderer(UInt32 width, UInt32 height);
+}
