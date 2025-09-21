26927c11b61a7c32c3b19a6901c8d2b28203bbd5
Ben Brougher <Ben Brougher>
2025-09-20 23:08:55 -0700

    wip: added more Zig ffi methods

--- packages/dotnet/Sst.Opentui.Core/Zig.cs
+++ packages/dotnet/Sst.Opentui.Core/Zig.cs
@@ -5,6 +5,92 @@ namespace Sst.Opentui.Core;
 //TODO: Make this insternal after testing
 public unsafe static partial class Zig
 {
-    [LibraryImport("opentui", EntryPoint = "createRenderer")]
+    public const string LIB_NAME = "opentui";
+
+    [LibraryImport(LIB_NAME, EntryPoint = "createRenderer")]
     public static partial IntPtr CreateRenderer(UInt32 width, UInt32 height);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "destroyRenderer")]
+    public static partial void DestroyRenderer(IntPtr renderer, [MarshalAs(UnmanagedType.I1)] bool useAlternateScreen, UInt32 splitHeight);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "setUseThread")]
+    public static partial void SetUseThread(IntPtr renderer, [MarshalAs(UnmanagedType.I1)] bool useThread);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "setBackgroundColor")]
+    public static partial void SetBackgroundColor(IntPtr renderer, IntPtr color);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "setRenderOffset")]
+    public static partial void SetRenderOffset(IntPtr renderer, UInt32 offset);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "updateStats")]
+    public static partial void UpdateStats(IntPtr renderer, double time, UInt32 fps, double frameCallbackTime);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "updateMemoryStats")]
+    public static partial void UpdateMemoryStats(IntPtr renderer, UInt32 heapUsed, UInt32 heapTotal, UInt32 arrayBuffers);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "render")]
+    public static partial void Render(IntPtr renderer, [MarshalAs(UnmanagedType.I1)] bool force);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "getNextBuffer")]
+    public static partial IntPtr GetNextBuffer(IntPtr renderer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "getCurrentBuffer")]
+    public static partial IntPtr GetCurrentBuffer(IntPtr renderer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "createOptimizedBuffer")]
+    public static partial IntPtr CreateOptimizedBuffer(UInt32 width, UInt32 height, [MarshalAs(UnmanagedType.I1)] bool respectAlpha, UInt16 widthMethod);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "destroyOptimizedBuffer")]
+    public static partial void DestroyOptimizedBuffer(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "drawFrameBuffer")]
+    public static partial void DrawFrameBuffer(IntPtr target, Int32 destX, Int32 destY, IntPtr frameBuffer, UInt32 sourceX, UInt32 sourceY, UInt32 sourceWidth, UInt32 sourceHeight);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "getBufferWidth")]
+    public static partial UInt32 GetBufferWidth(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "getBufferHeight")]
+    public static partial UInt32 GetBufferHeight(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferClear")]
+    public static partial void BufferClear(IntPtr buffer, IntPtr bg);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferGetCharPtr")]
+    public static partial IntPtr BufferGetCharPtr(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferGetFgPtr")]
+    public static partial IntPtr BufferGetFgPtr(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferGetBgPtr")]
+    public static partial IntPtr BufferGetBgPtr(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferGetAttributesPtr")]
+    public static partial IntPtr BufferGetAttributesPtr(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferGetRespectAlpha")]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static partial bool BufferGetRespectAlpha(IntPtr buffer);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferSetRespectAlpha")]
+    public static partial void BufferSetRespectAlpha(IntPtr buffer, [MarshalAs(UnmanagedType.I1)] bool respectAlpha);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferDrawText")]
+    public static partial void BufferDrawText(IntPtr buffer, IntPtr text, Int64 textLen, UInt32 x, UInt32 y, IntPtr fg, IntPtr bg, byte attributes);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferSetCellWithAlphaBlending")]
+    public static partial void BufferSetCellWithAlphaBlending(IntPtr buffer, UInt32 x, UInt32 y, UInt32 char_code, IntPtr fg, IntPtr bg, byte attributes);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferFillRect")]
+    public static partial void BufferFillRect(IntPtr buffer, UInt32 x, UInt32 y, UInt32 width, UInt32 height, IntPtr bg);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "bufferResize" )]
+    public static partial void BufferResize(IntPtr buffer, UInt32 newWidth, UInt32 newHeight);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "resizeRenderer")]
+    public static partial void ResizeRenderer(IntPtr renderer, UInt32 newWidth, UInt32 newHeight);
+
+    [LibraryImport(LIB_NAME, EntryPoint = "setCursorPosition")]
+    public static partial void SetCursorPosition(IntPtr renderer, UInt32 x, UInt32 y, [MarshalAs(UnmanagedType.I1)] bool visible);
+
+
 }
