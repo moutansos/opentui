c4ffb09352cb1f85096c0a1b1969f6c33db38610
Ben Brougher <Ben Brougher>
2025-09-21 22:04:12 -0700

    wip: kept adding more zig methods and removed some git specific files

--- packages/dotnet/Sst.Opentui.Examples/Program.cs
+++ packages/dotnet/Sst.Opentui.Examples/Program.cs
@@ -1,8 +1,12 @@
 using Sst.Opentui.Core;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 IntPtr renderer = Zig.CreateRenderer(800, 600);
+Zig.ClearTerminal(renderer);
+
+//Probably draw with renderer here instead at some point
+Console.WriteLine("Hello, World!");
+
 Console.ReadKey();
 Zig.DestroyRenderer(renderer, false, 0);
